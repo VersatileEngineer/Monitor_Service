a22abb5e6d222c3bba35cf973c25e1fabb942c31
krislender0104 <krislender0104@gmail.com>
2023-07-29 05:13:49 -0400

    feat: update config

--- config/config.go
+++ config/config.go
@@ -1,12 +1,34 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	BlockchainType string
 	RabbitMQURL    string
 	DatabaseURL    string
 }
 
-func LoadConfig() Config {
-	// Load configuration from environment variables or config file
-	// and return a Config struct
+func LoadConfig() (Config, error) {
+	viper.SetConfigFile("config.yaml") // Set the name of the config file (you can change it to your preferred format)
+
+	// Add any necessary configuration defaults here
+	viper.SetDefault("BlockchainType", "ethereum")
+	viper.SetDefault("RabbitMQURL", "amqp://guest:guest@localhost:5672/")
+	viper.SetDefault("DatabaseURL", "sqlite:///path/to/database.db")
+
+	if err := viper.ReadInConfig(); err != nil { // Read the config file and handle errors
+		return Config{}, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var cfg Config
+	err := viper.Unmarshal(&cfg) // Unmarshal the config file into a Config struct
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to unmarshal config file: %w", err)
+	}
+
+	return cfg, nil
 }
